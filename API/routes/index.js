2e340c9720d26a1e686c897a4862fb9bb74cd8ef
Sylvain Lasjunies <megasyl20@gmail.com>
2018-02-22 23:16:35 +0100

    ddd

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,13 +1,32 @@
 import express				from 'express';
-import usersRoutes			from './users';
+import { RES }				from 'eip808-sdk';
 
 const router = express.Router();
 
-router.get('/health-check', (req, res) => {
-    res.send('OK');
+router.post('/publishAvailability', async (req, res) => {
+    const RESContract = await RES;
+    const plop = await RESContract.publishOffer(req.body, '0xf17f52151ebef6c7334fad080c5704d77216b732', 'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f');
+    res.json(plop);
 });
 
-router.use('/users', usersRoutes);
+router.get('/ListAvailabilities', (req, res) => {
+    res.json('OK');
+});
 
+router.get('/ReadAvailability', (req, res) => {
+    res.json('OK');
+});
+
+router.post('/requestReservation', (req, res) => {
+    res.json('OK');
+});
+
+router.get('/ListReservations', (req, res) => {
+    res.json('OK');
+});
+
+router.get('/ReadReservation', (req, res) => {
+    res.json('OK');
+});
 
 export default router;
--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import createServer	from  './config/server';
 import dbConf		from  './config/db';
 import app		from  './config/app';
 import apiRoutes	from  './API/routes/index';
+import bodyParser	from  'body-parser';
 
 const port	= '3000';
 const server	= createServer(app, port);
-
+app.use(bodyParser.json());
 // mount routes
 app.use('/api', apiRoutes);
 
